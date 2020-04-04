48d8b25d7d26b3d16562145fe4e41507a5b71ced
denisfd <fedorchenko999@gmail.com>
2020-04-04 21:37:12 +0300

    Add kaitail

--- cmd/kaitail/kaitail.go
+++ cmd/kaitail/kaitail.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"regexp"
+
+	"github.com/openware/kaigara/pkg/broker"
+)
+
+var (
+	channel  = flag.String("c", "logs.*", "Redis channel pattern to subscribe")
+	showName = flag.Bool("n", false, "Show channel name")
+)
 
 func main() {
-	fmt.Println("hello")
+	flag.Parse()
+
+	client := broker.RedisClient()
+	pubsub := client.PSubscribe(*channel)
+	ch := pubsub.Channel()
+
+	re := regexp.MustCompile(`^Message<logs\.([A-z.]+?): (.+?)>$`)
+
+	for msg := range ch {
+		rs := re.FindStringSubmatch(msg.String())
+
+		if len(rs) < 2 {
+			log.Printf("Could not parse message: %s\n", msg)
+			continue
+		}
+
+		if *showName {
+			fmt.Printf("%s: %s\n", rs[1], rs[2])
+		} else {
+			fmt.Printf("%s\n", rs[2])
+		}
+	}
 }
