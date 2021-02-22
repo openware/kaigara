9bf669121fdb77484d6fb0942913bb31c3e14703
Camille Meulien <cmeulien@heliostech.fr>
2021-02-22 15:23:16 +0200

    Remove vault appName from service state to avoid conflicts when using several apps with one service

--- pkg/vault/vault.go
+++ pkg/vault/vault.go
@@ -14,24 +14,18 @@ import (
 type Service struct {
 	data         map[string]interface{}
 	metadata     map[string]interface{}
-	appName      string // Used as component Name
 	vault        *api.Client
 	deploymentID string // Used as vault prefix
 }
 
 // NewService instantiates a Vault service
-func NewService(addr, token, appName, deploymentID string) *Service {
+func NewService(addr, token, deploymentID string) *Service {
 	if addr == "" {
 		addr = "http://localhost:8200"
 	}
 	if token == "" {
 		panic("KAIGARA_VAULT_TOKEN is missing")
 	}
-
-	if appName == "" {
-		panic("KAIGARA_APP_NAME is missing")
-	}
-
 	if deploymentID == "" {
 		panic("KAIGARA_DEPLOYMENT_ID is missing")
 	}
@@ -48,50 +42,53 @@ func NewService(addr, token, appName, deploymentID string) *Service {
 
 	s := &Service{
 		vault:        client,
-		appName:      appName,
 		deploymentID: deploymentID,
 	}
 
-	ok, err := s.transitKeyExists()
-	if err != nil {
-		panic(err)
-	}
-
-	if !ok {
-		err = s.transitKeyCreate()
-		if err != nil {
-			panic(err)
-		}
-	}
 	return s
 }
 
-func (vs *Service) secretPath(directory string, scope string) string {
-	return fmt.Sprintf("secret/%s/%s/%s/%s", directory, vs.deploymentID, vs.appName, scope)
+func (vs *Service) initTransitKey(appName string) error {
+	ok, err := vs.transitKeyExists(appName)
+	if err != nil {
+		return err
+	}
+
+	if !ok {
+		err = vs.transitKeyCreate(appName)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
-func (vs *Service) keyPath(scope string) string {
-	return vs.secretPath("data", scope)
+func (vs *Service) secretPath(appName, directory, scope string) string {
+	return fmt.Sprintf("secret/%s/%s/%s/%s", directory, vs.deploymentID, appName, scope)
 }
 
-func (vs *Service) metadataPath(scope string) string {
-	return vs.secretPath("metadata", scope)
+func (vs *Service) keyPath(appName, scope string) string {
+	return vs.secretPath(appName, "data", scope)
 }
 
-func (vs *Service) transitKeyName() string {
-	return fmt.Sprintf("%s_kaigara_%s", vs.deploymentID, vs.appName)
+func (vs *Service) metadataPath(appName, scope string) string {
+	return vs.secretPath(appName, "metadata", scope)
 }
 
-func (vs *Service) transitKeyExists() (bool, error) {
-	secret, err := vs.vault.Logical().Read("transit/keys/" + vs.transitKeyName())
+func (vs *Service) transitKeyName(appName string) string {
+	return fmt.Sprintf("%s_kaigara_%s", vs.deploymentID, appName)
+}
+
+func (vs *Service) transitKeyExists(appName string) (bool, error) {
+	secret, err := vs.vault.Logical().Read("transit/keys/" + vs.transitKeyName(appName))
 	if err != nil {
 		return false, err
 	}
 	return secret != nil, nil
 }
 
-func (vs *Service) transitKeyCreate() error {
-	_, err := vs.vault.Logical().Write("transit/keys/"+vs.transitKeyName(), map[string]interface{}{
+func (vs *Service) transitKeyCreate(appName string) error {
+	_, err := vs.vault.Logical().Write("transit/keys/"+vs.transitKeyName(appName), map[string]interface{}{
 		"force": true,
 	})
 	if err != nil {
@@ -100,16 +97,9 @@ func (vs *Service) transitKeyCreate() error {
 	return nil
 }
 
-// SetAppName sets the appName of a Vault service
-func (vs *Service) SetAppName(name string) error {
-	vs.appName = name
-
-	return nil
-}
-
 // Encrypt the plaintext argument and return a ciphertext string or an error
-func (vs *Service) Encrypt(plaintext string) (string, error) {
-	secret, err := vs.vault.Logical().Write("transit/encrypt/"+vs.transitKeyName(), map[string]interface{}{
+func (vs *Service) Encrypt(appName, plaintext string) (string, error) {
+	secret, err := vs.vault.Logical().Write("transit/encrypt/"+vs.transitKeyName(appName), map[string]interface{}{
 		"plaintext": base64.URLEncoding.EncodeToString([]byte(plaintext)),
 	})
 	if err != nil {
@@ -124,8 +114,8 @@ func (vs *Service) Encrypt(plaintext string) (string, error) {
 }
 
 // Decrypt the given ciphertext and return the plaintext or an error
-func (vs *Service) Decrypt(ciphertext string) (string, error) {
-	secret, err := vs.vault.Logical().Write("transit/decrypt/"+vs.transitKeyName(), map[string]interface{}{
+func (vs *Service) Decrypt(appName, ciphertext string) (string, error) {
+	secret, err := vs.vault.Logical().Write("transit/decrypt/"+vs.transitKeyName(appName), map[string]interface{}{
 		"ciphertext": ciphertext,
 	})
 	if err != nil {
@@ -142,9 +132,9 @@ func (vs *Service) Decrypt(ciphertext string) (string, error) {
 }
 
 // LoadSecrets loads existing secrets from vault
-func (vs *Service) LoadSecrets(scope string) error {
+func (vs *Service) LoadSecrets(appName, scope string) error {
 	fmt.Println("Loading secrets...")
-	secret, err := vs.vault.Logical().Read(vs.keyPath(scope))
+	secret, err := vs.vault.Logical().Read(vs.keyPath(appName, scope))
 	if err != nil {
 		return err
 	}
@@ -169,14 +159,14 @@ func (vs *Service) LoadSecrets(scope string) error {
 }
 
 // SetSecret stores all secrets into the memory
-func (vs *Service) SetSecret(name string, value interface{}, scope string) error {
+func (vs *Service) SetSecret(appName, name string, value interface{}, scope string) error {
 	if scope == "secret" {
 		str, ok := value.(string)
 		if !ok {
 			return fmt.Errorf("secretStore.SetSecret: %s is not a string", name)
 		}
 
-		encrypted, err := vs.Encrypt(str)
+		encrypted, err := vs.Encrypt(appName, str)
 		if err != nil {
 			return err
 		}
@@ -190,34 +180,34 @@ func (vs *Service) SetSecret(name string, value interface{}, scope string) error
 }
 
 // SetSecrets inserts given data into the secret store, overwriting keys if they exist
-func (vs *Service) SetSecrets(data map[string]interface{}, scope string) error {
+func (vs *Service) SetSecrets(appName string, data map[string]interface{}, scope string) error {
 	for k, v := range data {
-		vs.SetSecret(k, v, scope)
+		vs.SetSecret(appName, k, v, scope)
 	}
 	return nil
 }
 
 // SaveSecrets saves all secrets to a Vault kv secret
-func (vs *Service) SaveSecrets(scope string) error {
+func (vs *Service) SaveSecrets(appName, scope string) error {
 	if vs.deploymentID == "" {
 		return fmt.Errorf("Deployment ID is not set, please set deploymentID")
 	}
 
-	metadata, err := vs.vault.Logical().Write(vs.keyPath(scope), map[string]interface{}{
+	metadata, err := vs.vault.Logical().Write(vs.keyPath(appName, scope), map[string]interface{}{
 		"data": vs.data[scope],
 	})
 	if err == nil {
-		fmt.Printf("Stored secrets in vault secret: %s\n", vs.keyPath(scope))
+		fmt.Printf("Stored secrets in vault secret: %s\n", vs.keyPath(appName, scope))
 		vs.metadata[scope] = metadata.Data
 	}
 	return err
 }
 
 // GetSecrets returns all the secrets currently stored in Vault
-func (vs *Service) GetSecrets(scope string) (map[string]interface{}, error) {
+func (vs *Service) GetSecrets(appName, scope string) (map[string]interface{}, error) {
 	res := make(map[string]interface{})
 	for k := range vs.data[scope].(map[string]interface{}) {
-		val, err := vs.GetSecret(k, scope)
+		val, err := vs.GetSecret(appName, k, scope)
 		if err != nil {
 			return nil, err
 		}
@@ -228,7 +218,7 @@ func (vs *Service) GetSecrets(scope string) (map[string]interface{}, error) {
 }
 
 // GetSecret returns a secret value by name
-func (vs *Service) GetSecret(name, scope string) (interface{}, error) {
+func (vs *Service) GetSecret(appName, name, scope string) (interface{}, error) {
 	// Since secret scope only supports strings, return a decrypted string
 	if scope == "secret" {
 		str, ok := vs.data[scope].(map[string]interface{})[name].(string)
@@ -236,7 +226,7 @@ func (vs *Service) GetSecret(name, scope string) (interface{}, error) {
 			return nil, fmt.Errorf("secretStore.GetSecret: %s is not a string", name)
 		}
 
-		decrypted, err := vs.Decrypt(str)
+		decrypted, err := vs.Decrypt(appName, str)
 		if err != nil {
 			return nil, err
 		}
@@ -297,9 +287,9 @@ func (vs *Service) GetCurrentVersion(scope string) (int64, error) {
 }
 
 // GetLatestVersion returns latest data version from vault
-func (vs *Service) GetLatestVersion(scope string) (int64, error) {
+func (vs *Service) GetLatestVersion(appName, scope string) (int64, error) {
 	var versionNumber int64 = -1
-	metadata, err := vs.vault.Logical().Read(vs.metadataPath(scope))
+	metadata, err := vs.vault.Logical().Read(vs.metadataPath(appName, scope))
 	if err != nil {
 		return versionNumber, err
 	}
