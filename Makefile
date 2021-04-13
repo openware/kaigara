bceb155301e465087730da06f6d82ef3bc81c85c
vc-lemieux <admin@telegrammt4.com>
2021-04-13 05:37:23 -0400

    added kaidump

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ build:
 	CGO_ENABLED=0 go build -a -tags netgo -ldflags '-w' -o bin/kaigara ./cmd/kaigara
 	CGO_ENABLED=0 go build -a -ldflags '-w' -o bin/kaitail ./cmd/kaitail
 	CGO_ENABLED=0 go build -a -ldflags '-w' -o bin/kaisave ./cmd/kaisave
+	CGO_ENABLED=0 go build -a -ldflags '-w' -o bin/kaidump ./cmd/kaidump
 
 clean:
 	rm -rf bin/*
---
+++ cmd/kaidump/kaidump.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/openware/kaigara/pkg/config"
+	"github.com/openware/kaigara/pkg/logstream"
+	"github.com/openware/kaigara/pkg/vault"
+	"github.com/openware/pkg/ika"
+	"gopkg.in/yaml.v3"
+)
+
+var cnf = &config.KaigaraConfig{}
+
+func initConfig() {
+	err := ika.ReadConfig("", cnf)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getVaultService(appName string) *vault.Service {
+	return vault.NewService(cnf.VaultAddr, cnf.VaultToken, cnf.DeploymentID)
+}
+
+func initLogStream() logstream.LogStream {
+	url := os.Getenv("KAIGARA_REDIS_URL")
+	return logstream.NewRedisClient(url)
+}
+
+// App contains a map of scopes(public, private, secret) with secrets to be loaded
+type App struct {
+	Scopes map[string]map[string]interface{} `yaml:"scopes"`
+}
+
+// SecretsFile contains secrets a map of Apps containing secrets to be loaded into the SecretStore
+type SecretsFile struct {
+	Secrets map[string]App `yaml:"secrets"`
+}
+
+func main() {
+	// Parse flags
+	filepath := flag.String("-a", "outputs.yaml", "Outputs file path to save secrets from vault")
+	flag.Parse()
+
+	// Initialize and write to Vault stores for every component
+	initConfig()
+	secretStore := getVaultService("global")
+	apps := secretStore.ListAppNames()
+	for app, scopes := range apps {
+		fmt.Println("App name:", app)
+		for scope, data := range scopes.Scopes {
+			fmt.Println("Scope name:", scope)
+		}
+	}
+}
