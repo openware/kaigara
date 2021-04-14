d1f5bfa37ee57a9f555b236608bb6d3366be6cad
vc-lemieux <vlemieux1984@gmail.com>
2021-04-14 21:08:53 +0000

    Added DeleteSecret function in vault pkg

--- pkg/vault/vault.go
+++ pkg/vault/vault.go
@@ -368,3 +368,21 @@ func (vs *Service) GetLatestVersion(appName, scope string) (int64, error) {
 	}
 	return versionNumber, nil
 }
+
+// Delete key from Data, Metadata and vault
+func (vs *Service) DeleteSecret(appName, name, scope string) error {
+	if vs.deploymentID == "" {
+		return fmt.Errorf("Deployment ID is not set, please set deploymentID")
+	}
+
+	metadata, err := vs.vault.Logical().Delete(vs.keyPath(appName, scope))
+	if err != nil {
+		return err
+	}
+	if metadata != nil {
+		vs.metadata[appName][scope] = metadata.Data
+	}
+	delete(vs.data[appName][scope].(map[string]interface{}), name)
+	err = vs.SaveSecrets(appName, scope)
+	return err
+}
