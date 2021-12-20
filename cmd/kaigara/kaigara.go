6bb3e047bc916601154d13364ca4f98bbd02e18b
Anton Filonenko <filonenko100075@gmail.com>
2021-12-20 13:08:49 +0200

    Feature: Restart child process instead of parent

--- cmd/kaigara/kaigara.go
+++ cmd/kaigara/kaigara.go
@@ -11,6 +11,7 @@ import (
 	"path"
 	"strings"
 	"sync"
+	"syscall"
 	"time"
 
 	"github.com/openware/kaigara/pkg/config"
@@ -46,90 +47,99 @@ func appNamesToLoggingName() string {
 }
 
 func kaigaraRun(ls logstream.LogStream, secretStore types.SecretStore, cmd string, cmdArgs []string) {
-	log.Printf("Starting command: %s %v", cmd, cmdArgs)
-	scopes := parseScopes()
-	c := exec.Command(cmd, cmdArgs...)
-	env := config.BuildCmdEnv(parseAppNames(), secretStore, os.Environ(), scopes)
+	for {
+		log.Printf("Starting command: %s %v", cmd, cmdArgs)
+		scopes := parseScopes()
+		c := exec.Command(cmd, cmdArgs...)
+		env := config.BuildCmdEnv(parseAppNames(), secretStore, os.Environ(), scopes)
 
-	c.Env = env.Vars
+		c.Env = env.Vars
 
-	for _, file := range env.Files {
-		os.MkdirAll(path.Dir(file.Path), 0750)
-		err := ioutil.WriteFile(file.Path, []byte(file.Content), 0640)
+		for _, file := range env.Files {
+			os.MkdirAll(path.Dir(file.Path), 0750)
+			err := ioutil.WriteFile(file.Path, []byte(file.Content), 0640)
+			if err != nil {
+				panic(fmt.Sprintf("Failed to write file %s: %s", file.Path, err.Error()))
+			}
+		}
+
+		stdin, err := c.StdinPipe()
 		if err != nil {
-			panic(fmt.Sprintf("Failed to write file %s: %s", file.Path, err.Error()))
+			panic(err)
 		}
-	}
 
-	stdin, err := c.StdinPipe()
-	if err != nil {
-		panic(err)
-	}
+		// Read STDIN and write it to the command
+		go func() {
+			r := bufio.NewReader(os.Stdin)
+			for {
+				line, isPrefix, err := r.ReadLine()
+				if err == io.EOF {
+					log.Printf("Reached EOF on STDIN")
+					stdin.Close()
+					break
+				} else if err != nil {
+					panic(err)
+				}
+				stdin.Write(line)
+				if !isPrefix {
+					stdin.Write([]byte("\n"))
+				}
+			}
+		}()
 
-	// Read STDIN and write it to the command
-	go func() {
-		r := bufio.NewReader(os.Stdin)
-		for {
-			line, isPrefix, err := r.ReadLine()
-			if err == io.EOF {
-				log.Printf("Reached EOF on STDIN")
-				stdin.Close()
-				break
-			} else if err != nil {
-				panic(err)
-			}
-			stdin.Write(line)
-			if !isPrefix {
-				stdin.Write([]byte("\n"))
-			}
+		stdout, err := c.StdoutPipe()
+		if err != nil {
+			log.Fatal(err)
 		}
-	}()
 
-	stdout, err := c.StdoutPipe()
-	if err != nil {
-		log.Fatal(err)
+		stderr, err := c.StderrPipe()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		channelOut := fmt.Sprintf("log.%s.%s", appNamesToLoggingName(), "stdout")
+		channelErr := fmt.Sprintf("log.%s.%s", appNamesToLoggingName(), "stderr")
+		log.Printf("Publishing on %s and %s\n", channelOut, channelErr)
+
+		var wg sync.WaitGroup
+		wg.Add(3)
+		go func() {
+			ls.Publish(channelOut, stdout)
+			wg.Done()
+		}()
+
+		go func() {
+			ls.Publish(channelErr, stderr)
+			wg.Done()
+		}()
+
+		if err := c.Start(); err != nil {
+			log.Fatal(err)
+		}
+
+		restart := make(chan int, 1)
+		go exitWhenSecretsOutdated(c, secretStore, scopes, restart)
+
+		quit := make(chan int)
+		go func() {
+			ls.HeartBeat(appNamesToLoggingName(), quit)
+			wg.Done()
+		}()
+
+		if err := c.Wait(); err != nil {
+			select {
+			case <-restart:
+				continue
+			default:
+				log.Fatal(err)
+			}
+			log.Fatal(err)
+		}
+		log.Printf("exit status 0\n")
+		quit <- 0
+
+		wg.Wait()
 	}
-
-	stderr, err := c.StderrPipe()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	channelOut := fmt.Sprintf("log.%s.%s", appNamesToLoggingName(), "stdout")
-	channelErr := fmt.Sprintf("log.%s.%s", appNamesToLoggingName(), "stderr")
-	log.Printf("Publishing on %s and %s\n", channelOut, channelErr)
-
-	var wg sync.WaitGroup
-	wg.Add(3)
-	go func() {
-		ls.Publish(channelOut, stdout)
-		wg.Done()
-	}()
-
-	go func() {
-		ls.Publish(channelErr, stderr)
-		wg.Done()
-	}()
-
-	if err := c.Start(); err != nil {
-		log.Fatal(err)
-	}
-
-	go exitWhenSecretsOutdated(c, secretStore, scopes)
-
-	quit := make(chan int)
-	go func() {
-		ls.HeartBeat(appNamesToLoggingName(), quit)
-		wg.Done()
-	}()
-
-	if err := c.Wait(); err != nil {
-		log.Fatal(err)
-	}
-	log.Printf("exit status 0\n")
-	quit <- 0
-
-	wg.Wait()
 }
 
 func initLogStream() logstream.LogStream {
@@ -137,7 +147,7 @@ func initLogStream() logstream.LogStream {
 	return logstream.NewRedisClient(url)
 }
 
-func exitWhenSecretsOutdated(c *exec.Cmd, secretStore types.SecretStore, scopes []string) {
+func exitWhenSecretsOutdated(c *exec.Cmd, secretStore types.SecretStore, scopes []string, restart chan int) {
 	appNames := append(parseAppNames(), "global")
 
 	if ignore, ok := os.LookupEnv("KAIGARA_IGNORE_GLOBAL"); ok && ignore == "true" {
@@ -159,8 +169,9 @@ func exitWhenSecretsOutdated(c *exec.Cmd, secretStore types.SecretStore, scopes 
 				}
 				if current != latest {
 					log.Printf("Found secrets updated on '%v' scope. from: v%v, to: v%v. killing process...\n", scope, current, latest)
-					if err := c.Process.Kill(); err != nil {
-						log.Fatal("Failed to kill process", err)
+					restart <- 0
+					if err := syscall.Kill(c.Process.Pid, syscall.SIGTERM); err != nil {
+						log.Fatalf("Failed to send interrupt signal to process: %s", err.Error())
 					}
 				}
 			}
